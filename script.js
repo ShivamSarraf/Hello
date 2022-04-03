23574824136a6429c8d4509f1e623234182bb709
ShivamSarraf <102786565+ShivamSarraf@users.noreply.github.com>
2022-04-03 14:49:30 +0530

    Add files via upload
    A Simple Website which provides you with To-Do List

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Select the submit button and input to be used later
+  const submit = document.querySelector('#addButton');
+  const newTask = document.querySelector('.inputText');
+
+  // Disable submit button by default:
+  submit.disabled = true;
+
+  // Listen for input to be typed into the input field
+  newTask.onkeyup = () => {
+      if (newTask.value.length > 0) {
+          submit.disabled = false;
+      }
+      else {
+          submit.disabled = true;
+      }
+  }
+
+  // Listen for submission of form
+  document.querySelector('form').onsubmit = () => {
+
+      // Find the task the user just submitted
+      const task = newTask.value;
+
+      // Create a list item for the new task and add the task to it
+      const li = document.createElement('li');
+      li.innerHTML = task;
+
+      // Add new element to our unordered list:
+      document.querySelector('#myUL').append(li);
+
+      // Clear out input field:
+      newTask.value = '';
+
+      // Disable the submit button again:
+      submit.disabled = true;
+
+      // Stop form from submitting
+      return false; }
+
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+        }
+      }, 
+    false);
+  }
+);
